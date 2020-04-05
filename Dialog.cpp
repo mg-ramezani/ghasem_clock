63bfdf9fbb02142210875999bbee7716bd8e1d20
Ghasem Ramezani <g1999ramezani@gmail.com>
2020-04-06 00:52:11 +0430

    add clock

--- Dialog.cpp
+++ Dialog.cpp
@@ -1,11 +1,15 @@
 #include "Dialog.h"
 #include "./ui_Dialog.h"
 
+#include <thread>
+#include <chrono>
+
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    setWindowTitle("ghasem-clock");
 }
 
 Dialog::~Dialog()
@@ -13,3 +17,59 @@ Dialog::~Dialog()
     delete ui;
 }
 
+void Dialog::on_pushButton_start_clicked()
+{
+    using namespace std::chrono_literals;
+
+    if (ui->pushButton_start->text() == "Stop")
+    {
+        is_over = true;
+        __clock = {};
+    }
+    else
+    {
+        is_over = false;
+    }
+
+    std::thread([&]() {
+        ui->pushButton_start->setText("Stop");
+
+        while (!is_over)
+        {
+            while (is_pause)
+            {
+                std::this_thread::yield();
+            }
+
+            if (__clock.s != 59)
+            {
+                __clock.s++;
+            }
+            else if (__clock.s == 59)
+            {
+                __clock.s = 0;
+                if (__clock.m != 59)
+                {
+                    __clock.m++;
+                }
+                else if (__clock.m == 59)
+                {
+                    __clock.m = 0;
+                    __clock.h++;
+                }
+            }
+
+            const QString t{QString("%1:%2:%3").arg(__clock.h).arg(__clock.m).arg(__clock.s)};
+            ui->label_time->setText(t);
+
+            std::this_thread::sleep_for(10ms);
+        }
+
+        ui->pushButton_start->setText("Start");
+    }).detach();
+}
+
+void Dialog::on_pushButton_2_pause_clicked()
+{
+    is_pause = !is_pause;
+}
--- Dialog.h
+++ Dialog.h
@@ -4,7 +4,10 @@
 #include <QDialog>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Dialog; }
+namespace Ui
+{
+class Dialog;
+}
 QT_END_NAMESPACE
 
 class Dialog : public QDialog
@@ -15,7 +18,22 @@ public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
 
+private slots:
+    void on_pushButton_start_clicked();
+
+    void on_pushButton_2_pause_clicked();
+
+private:
+    struct Clock
+    {
+        size_t s;
+        size_t m;
+        size_t h;
+    } __clock{0, 0, 0};
+
 private:
     Ui::Dialog *ui;
+    bool is_pause{false};
+    bool is_over{false};
 };
 #endif // DIALOG_H
